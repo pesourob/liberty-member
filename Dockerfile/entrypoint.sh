fe84b657a005fa264985f168d1a225c471f2a400
pesourob <maxines@seznam.cz>
2024-11-29 06:46:52 +0100

    Update entrypoint.sh

--- Dockerfile/entrypoint.sh
+++ Dockerfile/entrypoint.sh
@@ -5,12 +5,3 @@
 
 # Join to collective command // !!!HOSTNAME in this case must be use controller hostname !!! Try create configmap or secret
 ./collective join member1 --host=websphere-liberty-controller --port=9443 --keystorePassword=password123 --user=admin --password=password123 --autoAcceptCertificates
-
-#./collective join member1 --host=websphere-liberty-controller-0.websphere-liberty-controller.pesourob-was-liberty.svc.cluster.local --port=9443 --keystorePassword=password123 --user=admin --password=password123
-
-
-# Start the server
-/opt/ibm/wlp/bin/server start member1
-
-# Container run 
-tail -f /logs/messages.log
