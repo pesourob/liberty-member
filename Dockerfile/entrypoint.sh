5598827bd7e10adcecc9d00a873b580a6dc22d20
pesourob <maxines@seznam.cz>
2024-11-29 23:08:28 +0100

    Update entrypoint.sh

--- Dockerfile/entrypoint.sh
+++ Dockerfile/entrypoint.sh
@@ -5,3 +5,5 @@
 # Spustit kolektivní příkaz
 echo "joining to collective"
 /opt/ibm/wlp/bin/collective join member1 --host=websphere-liberty-controller --port=9443 --keystorePassword=password123 --user=admin --password=password123 --autoAcceptCertificates
+
+/opt/ibm/wlp/bin/server start member1 && tail -f /logs/messages.log 
