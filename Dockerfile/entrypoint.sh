a8d5fcdca4b045c87b1ab9ef6f65bbce2dd4579d
pesourob <maxines@seznam.cz>
2024-12-01 00:00:27 +0100

    Update entrypoint.sh

--- Dockerfile/entrypoint.sh
+++ Dockerfile/entrypoint.sh
@@ -2,10 +2,10 @@
 
 if [[ ! -e /opt/ibm/wlp/usr/servers/member1/resources || ! -e /opt/ibm/wlp/usr/servers/member1/collective-join-include.xml ]]; then
     echo "------------joining to collective------------"
-    /opt/ibm/wlp/bin/collective join member1 --host=websphere-liberty-controller --port=9443 --keystorePassword=password123 --user=admin --password=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/member1/ --autoAcceptCertificates
+   # /opt/ibm/wlp/bin/collective join member1 --host=websphere-liberty-controller --port=9443 --keystorePassword=password123 --user=admin --password=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/member1/ --autoAcceptCertificates
 else
     echo "------------Files exist, skipping collective join command------------"
 fi
 
 echo "------------Starting server------------"
-/opt/ibm/wlp/bin/server start member1 && tail -f /logs/messages.log
+#/opt/ibm/wlp/bin/server start member1 && tail -f /logs/messages.log
